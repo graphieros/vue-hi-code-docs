8de7ea9fe4b297dd9c5f4e35ab0399ec0756216c
graphieros <alec.lloyd.probert@gmail.com>
2025-01-31 08:13:22 +0100

    Update home page

--- src/components/ConfirmCopy.vue
+++ src/components/ConfirmCopy.vue
@@ -10,7 +10,7 @@ const isCopy = computed(() => store.isCopy);
 
 <template>
     <div style="z-index:10000000" v-if="isCopy" class="animate-ping origin-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col place-items-center gap-2 rounded-full">    
-        <div style="z-index:10000000" v-if="isCopy" class="animate-pulse h-[100px] w-[100px] flex place-items-center place-content-center ">
+        <div style="z-index:10000000" v-if="isCopy" class="animate-pulse h-[100px] w-[100px] flex place-items-center place-content-center">
             <VueUiIcon name="copyLeft" :size="100" stroke="#CD9077"/>
         </div>
     </div>
